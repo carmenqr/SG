06e382a215e226caf055972e600dd21d7993bfb7
carmenqr <e.carmenquilesr@go.ugr.es>
2024-04-15 16:36:16 +0200

    Coche cargado

---
+++ Juego/Juego.js
@@ -0,0 +1,88 @@
+
+import * as THREE from 'three'
+import { MTLLoader } from '../libs/MTLLoader.js'
+import { OBJLoader } from '../libs/OBJLoader.js'
+
+class Juego extends THREE.Object3D {
+  constructor(gui,titleGui) {
+    super();
+    
+    // Se crea la parte de la interfaz que corresponde a la grapadora
+    // Se crea primero porque otros métodos usan las variables que se definen para la interfaz
+    this.createGUI(gui,titleGui);
+    
+    // El material se usa desde varios métodos. Por eso se alamacena en un atributo
+    this.material = new THREE.MeshNormalMaterial();
+    this.material.flatShading = true;
+    this.material.needsUpdate = true;
+
+    this.circuito = this.createCircuito();
+    this.createCoche();
+    
+    this.add(this.circuito);
+  }
+
+  createCircuito(){
+
+    var pts = [
+      new THREE.Vector3(-5, -2, 15), 
+      new THREE.Vector3(1, -2, 10), //1
+      new THREE.Vector3(-2, 10, -5), //2
+      new THREE.Vector3(3, -1, -7),
+      new THREE.Vector3(3, -5, 0), //3
+      new THREE.Vector3(-3, 3, 6), //4
+      new THREE.Vector3(-6, 2, 20), //5
+      new THREE.Vector3(3, -3, 15), //8
+      new THREE.Vector3(7, 3, 10), //6
+      new THREE.Vector3(-15, 10, 8), //7
+      new THREE.Vector3(-10, 3, 2) //9
+    ];
+
+    // Crear la curva de Catmull-Rom cerrada
+    var path = new THREE.CatmullRomCurve3(pts, true);
+
+    // Resolución del tubo
+    var resolution = 200;
+
+    // Radio del tubo
+    var tubeRadius = 0.5;
+
+    // Segmentos que forman el círculo alrededor de la curva
+    var segments = 20;
+
+    // Crear la geometría del tubo cerrado
+    var tubeGeometry = new THREE.TubeGeometry(path, resolution, tubeRadius, segments, true);
+
+
+    // Crear una malla utilizando la geometría y el material
+    var forma = new THREE.Mesh(tubeGeometry, this.material);
+    return forma;
+
+  }
+
+  createCoche(){
+    var materialLoader = new MTLLoader();
+    var objectLoader = new OBJLoader();
+    materialLoader.load( '../models/coche2/LEGO_CAR_B2.mtl' ,
+      (materials) => {
+        objectLoader.setMaterials(materials);
+        objectLoader.load( '../models/coche2/LEGO_CAR_B2.obj' ,
+          (object) => {
+            object.scale.set(0.01, 0.01, 0.01); 
+            this.add(object);
+          }, null, null);
+      });
+  }
+  
+
+  createGUI(gui, titleGui) {
+    
+  }
+  
+
+  update () {
+    // No hay nada que actualizar ya que la apertura de la grapadora se ha actualizado desde la interfaz
+  }
+}
+
+export { Juego }
