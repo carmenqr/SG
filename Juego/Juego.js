4873bcdfe91be5de1a869e6bf8a7344fb8aa81a6
carmenqr <e.carmenquilesr@go.ugr.es>
2024-04-22 17:24:03 +0200

    Animacion BUENA del coche

--- Juego/Juego.js
+++ Juego/Juego.js
@@ -6,23 +6,26 @@ import { CSG } from '../libs/CSG-v2.js'
 import * as TWEEN from '../libs/tween.esm.js'
 
 class Juego extends THREE.Object3D {
-  constructor(gui,titleGui) {
+  constructor(gui, titleGui) {
     super();
-    
+
     // Se crea la parte de la interfaz que corresponde a la grapadora
     // Se crea primero porque otros métodos usan las variables que se definen para la interfaz
-    this.createGUI(gui,titleGui);
-    
+    this.createGUI(gui, titleGui);
+    this.t = 0.1;
+
     // El material se usa desde varios métodos. Por eso se alamacena en un atributo
     this.material = new THREE.MeshNormalMaterial();
     this.material.flatShading = true;
     this.material.needsUpdate = true;
 
     this.circuito = this.createCircuito();
-    this.createCoche();
+    //this.createCoche();
+
     // this.cubo = this.createCubo();
     this.puerta = this.createPuerta();
     
+    this.add(this.posicionOrientacionCoche());
     //this.add(this.cubo);
     this.add(this.puerta);
     this.add(this.circuito);
@@ -30,12 +33,13 @@ class Juego extends THREE.Object3D {
     this.animacionPuertas();
   }
 
-  createCubo(){
-    this.cuboGeometry = new THREE.BoxGeometry(0.5,0.5,0.5);
+  createCubo() {
+    this.cuboGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
     var material = new THREE.MeshStandardMaterial({ color: 0xffff00, emissive: 0xffff00, emissiveIntensity: 0.2 }); // Amarillo
     var cubo = new THREE.Mesh(this.cuboGeometry, material);
+    cubo.position.y = 0.25;
 
-    var posIni = this.path.getPointAt(0.2);
+    /* var posIni = this.path.getPointAt(0.2);
     
     var tangente = this.path.getTangentAt(0.2);
     var normal = new THREE.Vector3();
@@ -46,16 +50,16 @@ class Juego extends THREE.Object3D {
     cubo.position.copy(posIni);
 
     cubo.up = normal;
-    cubo.lookAt(posIni.clone().add(tangente));
+    cubo.lookAt(posIni.clone().add(tangente)); */
 
     return cubo;
 
   }
 
-  createCircuito(){
+  createCircuito() {
 
     var pts = [
-      new THREE.Vector3(-5, -2, 15), 
+      new THREE.Vector3(-5, -2, 15),
       new THREE.Vector3(1, -2, 10), //1
       new THREE.Vector3(-2, 10, -5), //2
       new THREE.Vector3(3, -1, -7),
@@ -90,24 +94,24 @@ class Juego extends THREE.Object3D {
 
   }
 
-  createCoche(){
-    this.t = 0;
+  createCoche() {
     var materialLoader = new MTLLoader();
     var objectLoader = new OBJLoader();
-    materialLoader.load( '../models/coche2/LEGO_CAR_B2.mtl' ,
+    materialLoader.load('../models/coche2/LEGO_CAR_B2.mtl',
       (materials) => {
         objectLoader.setMaterials(materials);
-        objectLoader.load( '../models/coche2/LEGO_CAR_B2.obj' ,
+        objectLoader.load('../models/coche2/LEGO_CAR_B2.obj',
           (object) => {
             object.scale.set(0.01, 0.01, 0.01);
-            this.coche = object; 
-            this.add(this.coche); 
-            
+            this.coche = object;
+
+            this.posCoche.add(this.coche);
+
           }, null, null);
       });
   }
 
-  createMarcos(){
+  createMarcos() {
 
     var marcos = new THREE.Object3D();
 
@@ -126,10 +130,10 @@ class Juego extends THREE.Object3D {
     marco_latdMesh.position.set(0.225, 0.5, 0);
     marco_latiMesh.position.set(-0.225, 0.5, 0);
 
-    marco_supMesh.rotation.set(0, 0, 90*(Math.PI/180));
+    marco_supMesh.rotation.set(0, 0, 90 * (Math.PI / 180));
     marco_supMesh.position.set(0, 1, 0);
 
-    
+
     var csg = new CSG();
     csg.union([marco_latdMesh, marco_latiMesh, marco_supMesh])
 
@@ -141,8 +145,8 @@ class Juego extends THREE.Object3D {
   }
 
 
-  createPuertaIzq(){
-  
+  createPuertaIzq() {
+
     var PuertaIzq = new THREE.Object3D();
 
     var forma = new THREE.BoxGeometry(0.2, 1, 0.05);
@@ -158,8 +162,8 @@ class Juego extends THREE.Object3D {
 
   }
 
-  createPuertaDcha(){
-  
+  createPuertaDcha() {
+
     var PuertaDcha = new THREE.Object3D();
 
     var forma = new THREE.BoxGeometry(0.2, 1, 0.05);
@@ -175,7 +179,7 @@ class Juego extends THREE.Object3D {
 
   }
 
-  createPuerta(){
+  createPuerta() {
     var puertas = new THREE.Object3D();
 
     var marc = this.createMarcos();
@@ -200,11 +204,11 @@ class Juego extends THREE.Object3D {
     return puertas;
   }
 
-  animacionPuertas(){
+  animacionPuertas() {
     const duracion = 2500;
 
-    var origen = {rotacion: 0};
-    var destino = {rotacion: -Math.PI/2};
+    var origen = { rotacion: 0 };
+    var destino = { rotacion: -Math.PI / 2 };
 
     var movimiento = new TWEEN.Tween(origen).to(destino, duracion).yoyo(true).repeat(Infinity);
 
@@ -216,37 +220,68 @@ class Juego extends THREE.Object3D {
 
   }
 
-  setAngulo (valor) {
+  setAngulo(valor) {
     this.pIzq.rotation.y = valor;
     this.pDcha.rotation.y = -valor;
   }
 
-  avanzarCoche(){
+  avanzarCoche(valor) {
     // asegurarse de que el coche se ha cargado antes de actualizar su posición
-     var posIni = this.path.getPointAt(this.t);
-     var tangente = this.path.getTangentAt(this.t);
-     var normal = new THREE.Vector3();
-     normal.crossVectors(tangente, this.path.getTangentAt(this.t + 0.001)).normalize();
-     var offset = normal.clone().multiplyScalar(this.tubeRadius);
-     posIni.add(offset);
-
-     this.coche.position.copy(posIni);
-     this.coche.up = normal;
-     this.coche.lookAt(posIni.clone().add(tangente));
- }
+    var posTmp = this.path.getPointAt(valor);
+    this.posOrCoche.position.copy(posTmp);
   
-  createGUI (gui,titleGui) {
+    var tangente = this.path.getTangentAt(valor);
+    posTmp.add(tangente);
+    var segmentoActual = Math.floor(valor*this.segments);
+    this.posOrCoche.up = this.tubeGeometry.binormals[segmentoActual];
+    this.posOrCoche.lookAt(posTmp);
+  }
+
+  posicionOrientacionCoche() {
+    this.posOrCoche = new THREE.Object3D();
+
+    var orientacion = this.orientacionCoche();
+
+    this.posOrCoche.add(orientacion);
+    this.avanzarCoche(this.t);
+    return this.posOrCoche;
 
   }
-  
-  update () {
+
+  orientacionCoche() {
+    var orCoche = new THREE.Object3D();
+
+    var posicion = this.posicionCoche();
+    orCoche.add(posicion);
+
+    orCoche.rotation.z = 0 *(Math.PI / 180);
+
+    return orCoche;
+  }
+
+  posicionCoche() {
+    this.posCoche = new THREE.Object3D();
+
+    this.createCoche();
+    this.posCoche.position.y = this.tubeRadius;
+
+    return this.posCoche;
+  }
+
+  createGUI(gui, titleGui) {
+
+  }
+
+  update() {
     TWEEN.update();
     this.t += 0.001;
     this.t = parseFloat(this.t.toFixed(3));
-    if (this.t >= 1) this.t = 0; 
+    //this.posicionOrientacionCoche();
+    if (this.t >= 1) this.t = 0;
     if (this.coche) {
-      this.avanzarCoche();
+      this.avanzarCoche(this.t);
     }
+    //this.posicionOrientacionCoche();
   }
 }
 
